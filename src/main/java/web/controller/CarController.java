94ab8d18bb1b382707ccec08911a5c417c044e66
skystar96 <skystar_uz@mail.ru>
2022-07-04 12:33:46 +0300

    C2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,10 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.Service;
 import web.service.ServiceImpl;
 
 import java.util.ArrayList;
@@ -12,13 +14,17 @@ import java.util.List;
 
 @Controller
 public class CarController {
+
+    @Autowired
+    private ServiceImpl service = new ServiceImpl();
+
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(
             value = "count",
             defaultValue = "5",
             required = false) int count, Model model) {
 
-        List<Car> carList = new ServiceImpl().getCar(count);
+        List<Car> carList = service.getCar(count);
         model.addAttribute("cars", carList);
 
         return "cars";
--- src/main/java/web/service/ServiceImpl.java
+++ src/main/java/web/service/ServiceImpl.java
@@ -1,18 +1,20 @@
 package web.service;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class ServiceImpl implements Service{
     @Override
     public List<Car> getCar(int count) {
-        Car car1 = new Car("AUDI", "black", 400);
-        Car car2 = new Car("BMW", "white",  700);
-        Car car3 = new Car("Mercedes-Benz", "grey",  1000);
-        Car car4 = new Car("Ford", "brown",  300);
-        Car car5 = new Car("Toyota", "green",  500);
+        Car car1 = new Car("AUDI", "чёрный", 400);
+        Car car2 = new Car("BMW", "белый",  700);
+        Car car3 = new Car("Mercedes-Benz", "серый",  1000);
+        Car car4 = new Car("Ford", "коричневый",  300);
+        Car car5 = new Car("Toyota", "зелёный",  500);
 
         List<Car> carList = new ArrayList<>();
         carList.add(car1);
