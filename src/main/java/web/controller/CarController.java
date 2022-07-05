e8f4dcab14e38600ca74c08aee6d19cca6fbd978
skystar96 <skystar_uz@mail.ru>
2022-07-05 11:39:26 +0300

    C3

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,28 +1,22 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.Service;
 import web.service.ServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @Autowired
-    private ServiceImpl service = new ServiceImpl();
-
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(
             value = "count",
             defaultValue = "5",
-            required = false) int count, Model model) {
+            required = false) int count, Model model, ServiceImpl service) {
 
         List<Car> carList = service.getCar(count);
         model.addAttribute("cars", carList);
